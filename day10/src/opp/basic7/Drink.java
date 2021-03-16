c48968b696f6827c7ea0618e8e708e7dc7986f90
KC-HaroldLee <80090106+KC-HaroldLee@users.noreply.github.com>
2021-03-16 11:23:47 +0900

    007 문제 해결

--- day10/src/opp/basic7/Drink.java
+++ day10/src/opp/basic7/Drink.java
@@ -17,13 +17,29 @@ public class Drink {
 
 	// 출력 메소드
 	void result() {
-		System.out.println(this.name);
-		System.out.println(this.type);
-		System.out.println(this.price);
-		if (eventOn == true) {
+		System.out.println("<메뉴정보>");
+		System.out.println("이름 : " + this.name);
+		System.out.println("종류 : " + this.type);
+		System.out.println("가격 : " + this.price);
+		if (this.eventOn) {
+			int dcPrice = this.price*(80/100);
 			System.out.println("행사중");
 		} else {
 			System.out.println("행사아님");
 		}
 	}
 }
+
+/*
+ * 
+ * 
+ * 	if (this.eventOn) {
+			int dcPrice = this.price*(80/100);
+			System.out.println("가격 " + dcPrice+"원(판매가 : " + this.price + "원");
+		} else {
+			System.out.println("행사아님");
+		}
+ */
+
+
+
