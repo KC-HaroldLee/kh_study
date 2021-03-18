1ec915beb1fed9e18020480a25ac0fd80f1d0426
KC-HaroldLee <80090106+KC-HaroldLee@users.noreply.github.com>
2021-03-18 10:33:09 +0900

    static 개념

---
+++ day13/src/oop/keyword4/Caculator.java
@@ -0,0 +1,16 @@
+package oop.keyword4;
+
+//계산기 : 사칙연산 도우미
+public class Caculator {
+	
+	// 계산기능 + static
+	// =탈 객체지향 키워드
+	// =클래스에 고정
+	public static int plus(int a, int b) {
+		return a + b;
+	}
+
+	public static int minus(int a, int b) {
+		return a - b;
+	}
+}
---
+++ day13/src/oop/keyword4/Test01.java
@@ -0,0 +1,9 @@
+package oop.keyword4;
+
+public class Test01 {
+public static void main(String[] args) {
+	
+	System.out.println(Caculator.plus(10,30));
+	System.out.println(Caculator.minus(10,30));
+}
+}
