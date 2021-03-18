6d335a471fc7ae7c1fc560a7e382c1ca3a1d1539
KC-HaroldLee <80090106+KC-HaroldLee@users.noreply.github.com>
2021-03-18 10:12:21 +0900

    변수를 바꿈

---
+++ day13/src/oop/keyword3/Caculator.java
@@ -0,0 +1,18 @@
+package oop.keyword3;
+
+//계산기 : 사칙연산 도우미
+public class Caculator {
+	// 변수 (일단 최소 두개)
+	// 자 과연 이 변수가 필요한가?
+	// private int first;
+	// private int second;
+
+	// 계산기능
+	public int plus(int a, int b) {
+		return a + b;
+	}
+
+	public int minus(int a, int b) {
+		return a - b;
+	}
+}
---
+++ day13/src/oop/keyword3/Test01.java
@@ -0,0 +1,15 @@
+package oop.keyword3;
+
+public class Test01 {
+public static void main(String[] args) {
+	
+	Caculator c = new Caculator();
+	
+	//c.setFirst(10);
+	//c.setSecond(15);
+	
+	
+	System.out.println(c.plus(10,30));
+	System.out.println(c.minus(10,30));
+}
+}
