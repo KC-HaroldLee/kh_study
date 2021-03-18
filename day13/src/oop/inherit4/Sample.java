713316f4c38f077e2ed6a5b5e4d9f147a787cdf8
KC-HaroldLee <80090106+KC-HaroldLee@users.noreply.github.com>
2021-03-18 15:14:59 +0900

    자바 API 활용 (와우)

---
+++ day13/src/oop/inherit4/Sample.java
@@ -0,0 +1,14 @@
+package oop.inherit4;
+import javax.swing.JFrame;
+import javax.swing.JColorChooser;
+import javax.swing.JFileChooser;
+//자식 클래스
+//부모님 자바에서 미리 만들어준 걸 쓴...다?? 그런 복지 정책이
+//public class Sample extends JFrame{
+
+//public class Sample extends JFileChooser{
+
+public class Sample extends JColorChooser{
+}
+
+
