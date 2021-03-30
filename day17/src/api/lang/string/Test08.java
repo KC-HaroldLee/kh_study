ce3516e03d6f8768af930606f25ea387118b6ece
user1 <user1@DESKTOP-AC3QINP>
2021-03-30 11:38:52 +0900

    형변환 연습합

--- day17/src/api/lang/string/Test08.java
+++ day17/src/api/lang/string/Test08.java
@@ -9,7 +9,7 @@ public class Test08 {
 	//String b = a;
 	//String b = (String)a;
 	//String b = ""+a; //편법 : 문자열은 어떤 값과 더해도 문자열이 된다는 특징을 이용
-	String b = String.valueOf(a); //정식방법
+	String b = String.valueOf(a); //정식 방법
 	
 	String c = "12345";
 	int d = Integer.parseInt(b); // 정식 방법
