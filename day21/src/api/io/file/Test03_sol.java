2ea85193a1c44166696e32948d5ff0549477c5ad
user1 <user1@DESKTOP-AC3QINP>
2021-03-31 18:05:01 +0900

    String 입출력

--- day21/src/api/io/file/Test03_sol.java
+++ day21/src/api/io/file/Test03_sol.java
@@ -17,9 +17,9 @@ public class Test03_sol {
 
 		List<File> list = new ArrayList<>();
 
-		for (char i = 'A'; i<='Z'; i++) {
+		for (char i = 'A'; i <= 'Z'; i++) {
 			File drive = new File(i + ":/");
-			
+
 			if (drive.exists()) {
 				System.out.println("<" + i + "드라이브의 정보>");
 				// 절대경로
@@ -37,24 +37,7 @@ public class Test03_sol {
 
 				System.out.println("-----------------------------");
 			}
-			
-			if (drive.exists()) {
-				System.out.println("<" + i + "드라이브의 정보>");
-				// 절대경로
-				System.out.println("절대경로 = " + drive.getPath());
-				// 수정날짜
-				Date d = new Date(drive.lastModified());
-				Format f = new SimpleDateFormat("Y년 M월 d일 E요일, a H:mm:ss");
-				System.out.println("마지막 수정시간 = " + f.format(d)); // !?!?!?!??!?!?!?!?!
-				// 파일크기
-				System.out.println("???(?)= " + drive.length());
-				// 읽기가능
-				System.out.println("읽기가능 = " + drive.canRead());
-				// 쓰기가능
-				System.out.println("쓰기가능 = " + drive.canWrite());
 
-				System.out.println("-----------------------------");
-			}
 		}
 	}
 
