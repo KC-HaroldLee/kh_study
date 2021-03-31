c68b462e469ba27ef2725f2c2570c45238756671
user1 <user1@DESKTOP-AC3QINP>
2021-03-31 17:54:18 +0900

    수정

---
+++ day19/src/api/util/date/Test04_01.java
@@ -0,0 +1,16 @@
+package api.util.date;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+public class Test04_01 {
+	public static void main(String[] args) {
+		
+		//지역설정
+		Date d = new Date();
+		
+		Format f = new SimpleDateFormat("yyyy-MM-dd E HH:mm:ss", Locale.KOREA);
+		
+		System.out.println(f.format(d));
+	}
+}
