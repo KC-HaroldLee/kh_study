ddaadb6c66cb424fd5232c0930081f884b72143a
KC-HaroldLee <80090106+KC-HaroldLee@users.noreply.github.com>
2021-04-01 17:42:51 +0900

    String 동일과 동등 테스트

--- day17/src/api/lang/string/Test02.java
+++ day17/src/api/lang/string/Test02.java
@@ -9,5 +9,9 @@ public class Test02 {
 		String c = new String("hello");
 		String d = new String("hello");
 				
+		System.out.println(a == b);//a와 b는 동일
+		System.out.println(b == c);//b와 c는 동일하지 않음
+		System.out.println(c == d);//c와 d는 동일하지 않음
+		System.out.println(a == d);//a와 d는 동일하지 않음
 	}
 }
