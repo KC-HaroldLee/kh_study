42db9b692e6275138e0cd23f6d8c2a9952436a34
KC-HaroldLee <80090106+KC-HaroldLee@users.noreply.github.com>
2021-04-01 17:41:49 +0900

    테스트

--- day02/src/Hello.java
+++ day02/src/Hello.java
@@ -1,14 +1,10 @@
-//import 는 준비 명령이다
-// '*'은 '전부'를 의미한다
-//  준비되지 않은 내용들은 사용할 수 없다.
-// 단 lang은 자동으로 써준다. 아하 ... 어쩐지.
 import java.lang.*; 
 
-public class Hello {//Hello.java를 대표하는 영역
-	public static void main(String[] args){//프로그램의 시작지점을 알린다.(main메소드) 
+public class Hello {
+	public static void main(String[] args){
 
-		//출력 명령
+
 		System.out.println("HelloJava!");
-		System.out.println("안녕자바!");
+
 	}
 }
