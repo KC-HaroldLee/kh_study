5145c32638e3358ef79b295db70edd0a3141e0d5
KC-HaroldLee <80090106+KC-HaroldLee@users.noreply.github.com>
2021-05-10 09:36:18 +0900

    .

--- jdbc1/src/connect/Test01.java
+++ jdbc1/src/connect/Test01.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Test01 {
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	public static void main(String[] args) throws Exception {
 		//JDBC Java Database Connectivity
 		//=자바에서 정보를 파일 대신 데이터베이스에 저장하기 위한 활동
 		//=데이터베이스에는 "구조화된 중요 정보"를 보관한다.
@@ -22,7 +22,7 @@ public class Test01 {
 				"jdbc:oracle:thin:@localhost:1521:xe", "kh", "kh");
 		System.out.println("성공?");
 		
-		
+
 		//연결종료
 		con.close();
 	}
